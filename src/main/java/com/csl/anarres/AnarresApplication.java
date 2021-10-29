f4ca26332b7f95ec56e275b57d3ea56f797c6cdf
chaishilin <592654362@qq.com>
2021-10-29 23:00:51 +0800

    add readme

--- src/main/java/com/csl/anarres/AnarresApplication.java
+++ src/main/java/com/csl/anarres/AnarresApplication.java
@@ -7,9 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.csl.anarres.mapper")
 public class AnarresApplication {
-	//todo 想着能不能填表格自动生成数据库中的表
+
 	public static void main(String[] args) {
 		SpringApplication.run(AnarresApplication.class, args);
 	}
-
 }
