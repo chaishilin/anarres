cbe945e460c16d41b2ff830ef26c289edd026720
chaishilin <592654362@qq.com>
2021-11-14 20:33:52 +0800

    针对幂等性接口注解的aop切面

--- src/main/java/com/csl/anarres/annotation/IdempotenceRequestAspect.java
+++ src/main/java/com/csl/anarres/annotation/IdempotenceRequestAspect.java
@@ -1,42 +1,56 @@
 package com.csl.anarres.annotation;
 
+import com.alibaba.fastjson.JSONObject;
+import com.csl.anarres.utils.HashcodeBuilder;
+import com.csl.anarres.utils.RedisUtil;
+import com.csl.anarres.utils.ResponseTemplate;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+
 /**
  * @author: Shilin Chai
  * @Date: 2021/11/8 19:11
  * @Description:
  */
-/*
+
 @Aspect
 @Component
-public class UserSelfOnlyAspect {
-    @Pointcut("execution(public * com.csl.anarres.controller.*.*(..))&& @annotation(com.csl.anarres.annotation.UserSelfOnly)")
-    public void addUserId(){
+public class IdempotenceRequestAspect {
+    Logger logger = LoggerFactory.getLogger(IdempotenceRequestAspect.class);
+    @Pointcut("execution(public * com.csl.anarres.controller.*.*(..)) && @annotation(com.csl.anarres.annotation.IdempotenceRequest)")
+    public void validRequest(){
     }//只是个函数签名，帮助记录的
-    @Around("addUserId()")
+
+    @Around("validRequest()")
     public Object Interceptor(ProceedingJoinPoint joinPoint) throws Throwable {
         Object result = null;
         Object[] args = joinPoint.getArgs();
-        if(args != null && args.length > 0){
-            for(Object arg : args){
-                if(arg.getClass().equals(ProgramDto.class) || arg.getClass().equals(ProgramEntity.class)){
-
-                    Field createrId =  arg.getClass().getDeclaredField("createrId");
-                    createrId.setAccessible(true);
-                    //System.out.println(createrId);
-                    createrId.set(arg,"123");
-
-
-                    //System.out.println(arg.toString());
+        if(args.length >= 1){
+            //如果请求是有参数的
+            Jedis jedis = RedisUtil.getInstance();
+            String argMD5 = HashcodeBuilder.getHashcode(args[0].toString());
+            System.out.println(argMD5);
+            String response = jedis.get(argMD5);
+            if(response != null){
+                //如果最近已经请求过，那么直接返回请求结果
+                logger.info("从缓存中直接返回请求:"+joinPoint.getSignature());
+                return JSONObject.toJavaObject(JSONObject.parseObject(response),ResponseTemplate.class);
+            }else{
+                //如果没有，则进行请求，并将响应计入缓存
+                try {
+                    result = joinPoint.proceed();
+                    jedis.setex(argMD5,(long)20,JSONObject.toJSONString(result));
+                }catch (Exception e){
+                    e.printStackTrace();;
                 }
             }
-        }
-        try {
-            //该方法只能做参数校验，无法改变参数
-            result = joinPoint.proceed();
-        }catch (Exception e){
-            e.printStackTrace();;
         }
         return result;
     }
 }
-*/
